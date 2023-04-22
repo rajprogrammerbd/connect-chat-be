4bf4c8de477e4749ba65979334880c355c735815
Raj Dutta <rd2249619@gmail.com>
2023-04-22 13:25:01 +0600

    initilize socket server

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,37 @@
-import express from "express";
+import dotenv from 'dotenv';
+dotenv.config();
 // import { v4 as uuidv4 } from 'uuid';
 // import UserLinkedList from './Data/users';
 // import MessageLinkedList, { IValues } from './Data/messages';
 // import { IPreparedDataType, ISocketsId, IUserTyping, IUsersName } from './types';
 // import { findTypingIdAvailable } from './helper';
-import { createServer } from "http";
-import { Server } from "socket.io";
-import consola from 'consola';
-import debug from 'debug';
-import cluster from 'node:cluster';
-import os from 'os';
+import { Server } from 'socket.io'
+import consola from 'consola'
+import debug from 'debug'
+import cluster from 'node:cluster'
+import os from 'os'
 
-const numCPUs = os.cpus().length;
+const numCPUs = os.cpus().length
 
 if (cluster.isPrimary) {
-
   for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
+    cluster.fork()
   }
-
 } else {
-    const app = express();
-    const httpServer = createServer(app);
-    const io = new Server(httpServer, { /* options */ });
-    
     // Debug logger.
-    const port_log = debug('listen:port');
-    const PORT = process.env.PORT || 3001;
+    const port_log = debug('listen:port')
+    const PORT = process.env.PORT || 3001
 
-    io.on("connection", () => {
-      // ...
-    });
-    
-    httpServer.listen(3000);
-    consola.success('Server is running');
-    port_log(`Server is running at http://localhost:${PORT}`);
+    const io = new Server(PORT as number, {
+        cors: {
+          origin: [`${process.env.FE_ENDPOINT_LINK}`],
+        },
+      })
+
+      io.on('connection', (socket) => {
+        socket.send('connected successfully');
+      });
+
+  consola.success('Server is running')
+  port_log(`Server is running at ws://localhost:${PORT}`)
 }
-
