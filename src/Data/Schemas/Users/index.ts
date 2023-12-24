8f3d6cd80366d7268ad577ebb5a9d310d27fef15
Raj Dutta <rd2249619@gmail.com>
2023-12-24 23:08:17 +0600

    add rules to user schema

--- src/Data/Schemas/Users/index.ts
+++ src/Data/Schemas/Users/index.ts
@@ -3,10 +3,28 @@ import mongoose from 'mongoose'
 const { Schema } = mongoose
 
 const userSchema = new Schema({
-  username: String,
-  email: String,
-  isRoot: Boolean,
-  connection_id: String,
+  username: {
+    type: String,
+    required: true,
+    min: 2,
+    max: 60
+},
+  email: {
+    type: String,
+    required: true,
+    min: 2,
+    max: 60
+  },
+  isRoot: {
+    type: Boolean,
+    required: true
+},
+  connection_id: {
+    type: String,
+    required: true,
+    min: 1,
+    max: 50
+  }
 })
 
 export default userSchema
