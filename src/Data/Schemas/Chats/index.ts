37b8666724e24a8043209263f786496c7bc56b9d
Raj Dutta <rd2249619@gmail.com>
2023-12-24 22:35:42 +0600

    add chat schema to the project

---
+++ src/Data/Schemas/Chats/index.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const chatSchema = new Schema({
+    email: {
+        messages: [
+            {
+                userId: String,
+                username: String,
+                message: String,
+                connection_id: String,
+                isRoot: Boolean
+            }
+        ]
+    }
+});
+
+export default chatSchema;
