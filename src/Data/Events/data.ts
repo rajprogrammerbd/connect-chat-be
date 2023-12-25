dcac48fe3f418646dfc2ebbd53c07c6421e623e3
Raj Dutta <rd2249619@gmail.com>
2023-12-25 23:49:39 +0600

    fixed adduser fn and update the connection basic template

--- src/Data/Events/data.ts
+++ src/Data/Events/data.ts
@@ -11,7 +11,7 @@ const connection = new Connection();
 
 // Add listeners to Data Class.
 connection.once('createConnection', () => {
-    const connection = mongoose.connect(process.env.DATABASE_URL!);
+    const connection = mongoose.connect(process.env.DATABASE_URL as string);
     connection
         .then(() => CONNECTION_STATUS(colors.bgGreen.italic("Database is connected")))
         .catch(() => CONNECTION_STATUS(colors.bgRed.white("Failed connect to database")))
