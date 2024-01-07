6be5ecc43a1855df8abc8a6fdf9e5a38b567d9c9
Raj Dutta <rd2249619@gmail.com>
2024-01-07 12:47:37 +0600

    remove comments and throw Error if data failed to connect

--- src/index.ts
+++ src/index.ts
@@ -53,34 +53,3 @@ const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => {
   console.log(`Server is listening on ${PORT}`);
 });
-
-/*
-io.on('connection', (socket) => {
-  console.log('connected with some client');
-  socket.on('create-user', (username: string, email: string, is_root: boolean, connection_id: null | string) => {
-    if (!username) {
-      socket.emit('failed-create-user', 'Username is required');
-    } else if (!email) {
-      socket.emit('failed-create-user', 'Email is required');
-    } else if (is_root === undefined) {
-      socket.emit('failed-create-user', 'is_root is required');
-    } else if (connection_id === undefined) {
-      socket.emit('failed-create-user', 'connection_id is required');
-    } else {
-      const user = data.addUser(username, email, is_root, connection_id);
-      console.log(user);
-    }
-  });
-});
-
-const PORT = process.env.PORT || 8000;
-server.listen(PORT, () => console.log(`Server is listening on port ${PORT}`))
-
-
-import { dirname } from 'node:path';
-
-
-d.addUser('Dola1 Dutta','doladutta5r@gmail.com', false, 'def73dd596d9eda12645')
-  .then(obj => console.log('result', obj))
-  .catch(res => console.error(res))
-*/
